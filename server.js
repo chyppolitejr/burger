cbb560a558b5a0bcbeec874858f2238a06043489
chyppoli <claude.hyppolite@gmail.com>
2020-12-03 13:34:00 -0500

    add code to server.js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require("express");
-const mysql = require("mysql");
-const app = express();
-const PORT = process.argv.PORT || 8080;
+var exphbs = require("express-handlebars");
+var express = require("express");
+var mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-app.listen(PORT, () => {
-  console.log("Example app listening on port port!");
-});
-
-//Run app, then load http://localhost:port in a browser to see the output.
