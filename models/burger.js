0f20775bfcd63c24fb9ea7bace8c66b02db9c00e
chyppoli <claude.hyppolite@gmail.com>
2020-12-03 17:29:36 -0500

    add code to burger.js to execut queries

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,22 @@
 const orm = require("../config/orm.js");
+
+const burger = {
+  selectAll: (cb) => {
+    orm.all("burgers", (res) => {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  InsertOne: (cols, vals, cb) => {
+    orm.insertOne("cats", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: (objColVals, condition, cb) => {
+    orm.updateOne("cats", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+};
+
+modules.export = burger;
