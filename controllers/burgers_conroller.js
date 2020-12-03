7c209eb51a6a6406ad32415e284910c8156af64e
chyppoli <claude.hyppolite@gmail.com>
2020-12-03 18:08:21 -0500

    add css file and assets directories. add some helper functions to orm. start controller file

---
+++ controllers/burgers_conroller.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// import model (burger.js) to use its database functions
+const burger = require("../models/burger.js");
+
+//Routes
+
+
+module.export = router;
