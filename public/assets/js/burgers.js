8979c7bf72f7372fb24e4cf3a22bf62fe88ed01f
chyppoli <claude.hyppolite@gmail.com>
2020-12-04 19:09:31 -0500

    add devoured template to style devoured list differently then burgers list

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -45,17 +45,4 @@ $(function () {
       location.reload();
     });
   });
-
-  //   $(".delete-cat").on("click", function (event) {
-  //     var id = $(this).data("id");
-
-  //     // Send the DELETE request.
-  //     $.ajax("/api/cats/" + id, {
-  //       type: "DELETE",
-  //     }).then(function () {
-  //       console.log("deleted cat", id);
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     });
-  //   });
 });
